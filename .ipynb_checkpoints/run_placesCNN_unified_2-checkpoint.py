5f2653461c27b6f405c7a7a457f33cfea8675765
Kavita D <kavitarajdeo@yahoo.com>
2019-10-31 01:13:04 +0530

    a

--- .ipynb_checkpoints/run_placesCNN_unified_2-checkpoint.py
+++ .ipynb_checkpoints/run_placesCNN_unified_2-checkpoint.py
@@ -188,5 +188,6 @@ height, width, _ = img_np.shape
 heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
 result = heatmap * 0.4 + img_np * 0.5
 cv2.imwrite('cam.jpg', result)
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
-cv2.imshow('cam.jpg', result)
+import matplotlib.pyplot as plt
+plt.imshow('cam.jpg')
+plt.show()
