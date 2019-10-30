23bb12b580278692254b1f751c4f7b28bd9bf542
Kavita D <kavitarajdeo@yahoo.com>
2019-10-31 00:55:08 +0530

    a

--- .ipynb_checkpoints/run_placesCNN_unified_2-checkpoint.py
+++ .ipynb_checkpoints/run_placesCNN_unified_2-checkpoint.py
@@ -188,7 +188,4 @@ height, width, _ = img_np.shape
 heatmap = cv2.applyColorMap(cv2.resize(CAMs[0],(width, height)), cv2.COLORMAP_JET)
 result = heatmap * 0.4 + img_np * 0.5
 cv2.imwrite('cam.jpg', result)
-
-import matplotlib.pyplot as plt
-plt.imshow(result)
-plt.show()
+cv2.imshow('cam.jpg', result)
